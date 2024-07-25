36ef17a074170738bfb7ea2079d7220d1954a4c5
Felix G Lopez <felixglopez@gmail.com>
2024-07-25 05:08:37 -0300

    ending of assignment

--- week2.R
+++ week2.R
@@ -18,9 +18,7 @@ unzip(zip_file)
 list.files()
 
 #loading the file
-library(readr)
 activity <- read_csv("activity.csv")
-View(activity)
 
 dim(activity)
 head(activity)
@@ -40,14 +38,14 @@ summary(ts$total_steps, na.rm=TRUE)
 
 #1. Make a time series
 ## Calculate the average number of steps per interval
-average_steps <- activity %>%
+avg_steps <- activity %>%
         group_by(interval) %>%
         summarize(avg_steps = mean(steps, na.rm = TRUE))
 
 str(average_steps)
 
 #Creating a times series plot
-ggplot(average_steps, aes(x = interval, y = avg_steps)) +
+ggplot(avg_steps, aes(x = interval, y = avg_steps)) +
         geom_line() +
         labs(title = "Average Number of Steps per 5-Minute Interval",
              x = "5-Minute Interval",
@@ -55,42 +53,85 @@ ggplot(average_steps, aes(x = interval, y = avg_steps)) +
 
 #Calculating which day has the maximum average step 
 # Find the interval with the maximum average steps
-max_interval <- average_steps %>%
+max_interval <- avg_steps %>%
         filter(avg_steps == max(avg_steps, na.rm = TRUE))
 
 print(max_interval)
 #ANSWER = interval 835
 
 
-#######IMPUTING MISSING VALUES
+#######IMPUTING MISSING VALUES FALTA FAZER!!!!!!!!!!!!!!
 
 ##1.calculating the total number of rows with missing values
 total.na <- sum(is.na(activity$steps))
 
 print(total.na)      
 
+
+
+
 # 2. Impute missing values with the previous line's information
-NA_filled <- activity %>%
-        fill(steps, .direction = "up")
+# Replace each missing value with the mean value of its 5-minute interval
+# Function to fill in the missing values
+# Separate valid and NA steps
+actv1 <- filter(actv, !is.na(steps))
+actv0 <- filter(actv, is.na(steps))
 
-# Print the result
-print(NA_filled)
-
-# Custom function to fill NA with previous non-NA and non-zero value
-fill_na_with_previous_nonzero <- function(x) {
-        last_value <- NA
-        for (i in seq_along(x)) {
-                if (is.na(x[i])) {
-                        x[i] <- last_value
-                } else if (x[i] != 0) {
-                        last_value <- x[i]
-                }
-        }
-        return(x)
+# Loop to fill NA steps with mean steps for the corresponding interval
+for (i in unique(actv0$interval)) {
+        actv0[actv0$interval == i, "steps"] <- avg_steps$avg_steps[avg_steps$interval == i]
 }
 
-# Apply the function to the 'steps' column
-NA_filled <- fill_na_with_previous_nonzero(activity$steps)
+# Combine the data back together
+filled_data <- bind_rows(actv1, actv0) %>%
+        arrange(date, interval)
 
 # Print the result
-print(NA_filled)
+head(filled_data)
+
+
+
+
+##7. Histogram of the total number of steps 
+##taken each day after missing values are imputed:
+
+
+agg_step_new <- aggregate(filled_data$steps, by = list(filled_data$date), FUN = "sum")
+hist(agg_step_new$x, col="gray", main = "Histogram plot of Total Number Steps", xlab = "Total Steps")
+
+
+# Re-calculate mean and median of Total number of steps per day after filling missing values
+mnval_new <- mean(agg_step_new$x)
+mdval_new <- median(agg_step_new$x)
+
+#ANSWER: Observation: The mean and median values are highrer after 
+#replacing missing values comparing original dataset.
+
+
+#8. Panel plot comparing the average number of steps taken per 
+#5-minute interval across weekdays and weekends
+
+
+# Add a new column 'day' to hold day of the week
+actv_new <- mutate(filled_data, day = weekdays(date))
+# Add day category as 'weekend' and 'weekday'
+actv_new$day_catg <- if_else(actv_new$day %in% c('Sunday','Saturday'), 'weekend', 'weekday')
+actv_new$day_catg <- factor(actv_new$day_catg)
+str(actv_new)
+
+
+#Make a panel plot containing a time series plot of the 
+#5-minute interval (x-axis) and the average number of steps taken, averaged across all weekday days or weekend days (y-axis).
+
+avg_step_week <- aggregate(steps ~ interval + day_catg, data = actv_new, FUN = "mean")
+head(avg_step_week)
+
+
+#creating the graph
+
+g <- ggplot(data = avg_step_week, aes(x = interval, y = steps))
+g <- g + facet_grid(day_catg ~ .)
+g <- g + geom_line()
+g <- g + labs(title = "Avg Steps taken across weekday/weekend per Interval")
+g <- g + labs(x = "Interval", y = "Average Steps")
+print(g)
